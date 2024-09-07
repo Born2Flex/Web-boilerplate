a9427edd39c72eae7c5c632c4ac7169807a48717
Danylo Shlapak <geografy2314@gmail.com>
2024-09-08 00:40:45 +0300

    added new hover effects, popups

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,14 @@ const scrollContainer = document.querySelector('.scroll');
 const leftBtn = document.querySelector('.scroll-btn.left');
 const rightBtn = document.querySelector('.scroll-btn.right');
 
+const addTeacherBtn = document.querySelectorAll('.add-teacher');
+const addTeacherPopup = document.querySelector('#add-teacher');
+const addTeacherCloseBtn = document.querySelector('#add-close-btn')
+const teacherInfoCloseBtn = document.querySelector('#info-close-btn')
+
+const teachersImages = document.querySelectorAll('.teacher-image')
+const teacherInfoPopup = document.querySelector('#teacher-info');
+
 leftBtn.addEventListener('click', () => {
   scrollContainer.scrollBy({
     left: -200,
@@ -21,4 +29,20 @@ rightBtn.addEventListener('click', () => {
     left: 200,
     behavior: 'smooth'
   });
+});
+
+addTeacherBtn.forEach(btn => btn.addEventListener('click', () => {
+  addTeacherPopup.classList.add('visible');
+}));
+
+teachersImages.forEach(teacher => teacher.addEventListener('click', () => {
+  teacherInfoPopup.classList.add('visible');
+}));
+
+addTeacherCloseBtn.addEventListener('click', () => {
+  addTeacherPopup.classList.remove('visible');
+});
+
+teacherInfoCloseBtn.addEventListener('click', () => {
+  teacherInfoPopup.classList.remove('visible');
 });
