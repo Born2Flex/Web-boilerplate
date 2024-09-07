3145ba9ec8df5843fcf93dab4a67893ac34920c8
Danylo Shlapak <geografy2314@gmail.com>
2024-09-07 15:11:07 +0300

    added table pagination, container with scroll

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,21 @@ require('../css/app.css');
 /** ******** Your code here! *********** */
 
 console.log(testModules.hello);
+
+const scrollContainer = document.querySelector('.scroll');
+const leftBtn = document.querySelector('.scroll-btn.left');
+const rightBtn = document.querySelector('.scroll-btn.right');
+
+leftBtn.addEventListener('click', () => {
+  scrollContainer.scrollBy({
+    left: -200,
+    behavior: 'smooth'
+  });
+});
+
+rightBtn.addEventListener('click', () => {
+  scrollContainer.scrollBy({
+    left: 200,
+    behavior: 'smooth'
+  });
+});
